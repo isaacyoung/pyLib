cf7d1f7c89956ba29ce937583494a83228a0a997
yzh <48515824@qq.com>
2018-11-20 18:49:14 +0800

    script

--- script/do.py
+++ script/do.py
@@ -3,9 +3,15 @@ import sys
 
 argCount = len(sys.argv)
 if argCount == 1:
+    print("python", "do.py", "-help")
     exit(0)
 
 key = sys.argv[1]
+if key in ["help","-help","--help"]:
+    print("params list:")
+    print("%10s"%"ch|cache", "run memcached", sep=" --> ")
+    exit(0)
+
 path = ''
 if key in ['ch','cache']:
     path = r"C:\Users\Administrator\Desktop\tools\memcached.exe"
--- script/to.py
+++ script/to.py
@@ -3,11 +3,18 @@ import sys
 
 argCount = len(sys.argv)
 if argCount == 1:
+    print("python", "to.py", "-help")
     exit(0)
 
 key = sys.argv[1]
+if key in ["help","-help","--help"]:
+    print("params list:")
+    print("%10s"%"dp|deply", "to deply file folder", sep=" --> ")
+    print("%10s"%"out", "to packup out file folder", sep=" --> ")
+    exit(0)
+
 path = "C:\\Users\\Administrator\\Desktop\\tools"
-if key in ['dp','deply']:
+if key in ['dp', 'deply']:
     path = "F:\\LIANLIAN_DAYLY\\生产环境\\升级申请\\升级文件列表"
 elif key == 'out':
     path = "F:\\output"
